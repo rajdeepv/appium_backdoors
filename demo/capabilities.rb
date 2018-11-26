75afa44eb9a652147b5c562f1d4e7bc93a0a8e3a
Rajdeep Varma <mail.rajvarma@gmail.com>
2018-11-26 00:41:35 +0000

    add ios app

--- demo/capabilities.rb
+++ demo/capabilities.rb
@@ -1,7 +1,7 @@
 def connected_devices
   lines = %x(adb devices).split("\n")
-  start_index = lines.index { |x| x =~ /List of devices attached/ } + 1
-  lines[start_index..-1].collect { |l| l.split("\t").first }
+  start_index = lines.index {|x| x =~ /List of devices attached/} + 1
+  lines[start_index..-1].collect {|l| l.split("\t").first}
 end
 
 APPIUM_FORWARDED_PORT = 4567
@@ -21,4 +21,15 @@ def android_caps
       systemPort: APPIUM_FORWARDED_PORT
   }
 
+end
+
+def ios_caps
+  {
+      platformName: 'ios',
+      deviceName: "iPhone 6",
+      app: 'TTL.app',
+      platformVersion: "11.2",
+      automationName: "XCUITest",
+      newCommandTimeout: 600,
+  }
 end
