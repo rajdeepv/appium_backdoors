8cab75c85bc726443afafba594779398e4e1c0b5
Rajdeep Varma <mail.rajvarma@gmail.com>
2018-11-29 20:14:42 +0000

    Use typed arguments

--- demo/scenario.rb
+++ demo/scenario.rb
@@ -10,10 +10,55 @@ reinstall_apps
 
 @driver.start_driver
 
+methods = {
+    target: 'activity',
+    methods:
+        [
+            {
+                name: "raiseToast",
+                args: [{value: "Love Appium", type: 'java.lang.String'}]
+            }
+        ]
+}
+
+methods = {
+    target: 'activity',
+    methods:
+        [
+            {
+                name: "messageView",
+            },
+            {
+                name: "append",
+                args:
+                    [
+                        {value: "Lol", type: 'java.lang.CharSequence'},
+                        {value: 1, type: 'int'},
+                        {value: 2, type: 'int'}
+                    ]
+            }
+        ]
+}
+
+
+
+methods = {
+    target: 'activity',
+    methods:
+        [
+            {
+                name: "messageView",
+            },
+            {
+                name: "getTextSize",
+            }
+        ]
+}
+
 
 binding.pry
 
-@driver.execute_script("mobile: backdoor",[{name:"messageView"},{name:"getTextSize"}])
-@driver.execute_script("mobile: backdoor",[{name:"raiseToast",args:["Love Appium"]}])
-@driver.execute_script("mobile: backdoor",[{name:"messageView"},{name:"getTypeface"},{name:"isItalic"}])
-@driver.execute_script("mobile: backdoor",[{name:"messageView"},{name:"setError",args:["Lol"]}])
+@driver.execute_script("mobile: backdoor", methods)
+@driver.execute_script("mobile: backdoor", [{name: "raiseToast", args: ["Love Appium"]}])
+@driver.execute_script("mobile: backdoor", [{name: "messageView"}, {name: "getTypeface"}, {name: "isItalic"}])
+@driver.execute_script("mobile: backdoor", [{name: "messageView"}, {name: "setError", args: ["Lol"]}])
