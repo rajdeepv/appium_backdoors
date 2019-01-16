15ef4c74877f36a130665f15d4a33ee941362ec4
Rajdeep Varma <mail.rajvarma@gmail.com>
2019-01-16 18:50:22 +0000

    uiautomator example

---
+++ demo/uiautomator.rb
@@ -0,0 +1,7 @@
+require_relative 'utils/env'
+
+@driver = Appium::Driver.new(caps: android_caps_espresso.merge(app: 'apidemo.apk'))
+@driver.start_driver
+
+require 'pry'; binding.pry
+@driver.execute_script("mobile:uiautomator", {strategy: 'textContains', value:'Access', index:1, action:'click'})
