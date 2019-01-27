761486cb542e2b9c934bdd42fea4d879317ad815
Rajdeep Varma <mail.rajvarma@gmail.com>
2019-01-27 19:24:52 +0000

    toggle button

--- demo/toggle_button.rb
+++ demo/toggle_button.rb
@@ -3,9 +3,12 @@ require_relative 'utils/env'
 @driver = Appium::Driver.new(caps: android_caps_espresso.merge(app: 'apidemo.apk'))
 @driver.start_driver
 
+@driver.find_element(xpath_by_text("Views")).click
+@driver.find_element(xpath_by_text("Buttons")).click
+
+e = @driver.find_element({id: 'button_toggle'})
 
 require 'pry'; binding.pry
-e = @driver.find_element({id: 'button_toggle'})
 @driver.execute_script("mobile: backdoor",
                        {
                            target: "element",
