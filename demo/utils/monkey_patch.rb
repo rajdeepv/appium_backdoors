01821bba012ae2ca8e9477c7de7ceffb450e9567
Rajdeep Varma <mail.rajvarma@gmail.com>
2019-05-20 16:13:18 +0100

    add flash to selenium patch

--- demo/utils/monkey_patch.rb
+++ demo/utils/monkey_patch.rb
@@ -1,20 +1,15 @@
-module MonkeyPatch
-  module Selenium
-    module WebDriver
-      module Element
-        def flash
-          bridge.flash(@id)
-        end
+module Selenium
+  module WebDriver
+    class Element
+      def flash
+        bridge.flash(@id)
       end
-
     end
   end
 end
 
-Selenium::WebDriver::Element.prepend(MonkeyPatch::Selenium::WebDriver::Element)
-
 Appium::Core::Base::Bridge::W3C.class_eval do
-  def flash(element) # rubocop:disable Naming/MethodName
+  def flash(element)
     execute :execute_script, {}, {script: "mobile: flashElement", args: {element: element}}
   end
 end
