f3bcea3cd512a18e056ee2c2cf19e9227b33a6b4
Rajdeep Varma <mail.rajvarma@gmail.com>
2019-05-29 17:29:16 +0100

    removing unused backdoor

--- demo/element_extention.rb
+++ demo/element_extention.rb
@@ -1,12 +1,8 @@
 require_relative 'utils/env'
 
 @driver = Appium::Driver.new({caps: android_caps_espresso}, false)
+
 @driver.start_driver
-
-
-id = @driver.find_element({id: 'seekBar'}).ref
-@driver.execute_script("mobile: backdoor", {target: "element", elementId: id, methods: [{name: "getProgress"}]})
-
 
 element = @driver.find_element({id: 'message'})
 
