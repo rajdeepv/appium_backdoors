58616e57ada1616f267a471239db5c919dbab103
Rajdeep Varma <mail.rajvarma@gmail.com>
2019-06-04 15:11:25 +0100

    add comments

--- demo/element_extention.rb
+++ demo/element_extention.rb
@@ -1,9 +1,12 @@
 require_relative 'utils/env'
 
+# Instantiate driver
 @driver = Appium::Driver.new({caps: android_caps_espresso}, false)
 
+# Start driver
 @driver.start_driver
 
+# Call element Backdoor
 color = @driver.execute_script("mobile: backdoor",
                                {
                                    target: "element",
