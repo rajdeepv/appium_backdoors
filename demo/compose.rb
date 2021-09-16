9a2d10f778d44872eafcb4cf939c3aeeaad3517c
Rajdeep Varma <mail.rajvarma@gmail.com>
2021-09-16 10:44:15 +0100

    reorganize capabilities

--- demo/compose.rb
+++ demo/compose.rb
@@ -1,6 +1,22 @@
 require_relative 'utils/env'
 
-@driver = Appium::Driver.new({caps: android_compose_caps}, false)
+def android_compose_caps(app: 'compose_playground.apk/')
+  {
+    platformName: 'Android',
+    deviceName: connected_devices.first,
+    app: File.join(File.dirname(__FILE__), '../' + app),
+    automationName: 'espresso',
+    newCommandTimeout: 0,
+    skipUnlock: true,
+    fullReset: false,
+    forceEspressoRebuild: true,
+    showGradleLog: true,
+    espressoBuildConfig: '{"additionalAndroidTestDependencies": ["androidx.lifecycle:lifecycle-extensions:2.2.0", "androidx.activity:activity:1.3.1", "androidx.fragment:fragment:1.3.4"]}'
+  }
+end
+
+@driver = Appium::Driver.new({ caps: android_compose_caps }, false)
+
 @driver.start_driver
 
 binding.pry
--- demo/utils/capabilities.rb
+++ demo/utils/capabilities.rb
@@ -34,18 +34,3 @@ def android_uia_caps
   }
 end
 
-def android_compose_caps(app: 'compose_playground.apk/')
-  {
-    platformName: 'Android',
-    deviceName: connected_devices.first,
-    app: File.join(File.dirname(__FILE__), '../../'+ app),
-    # appWaitActivity: '*',
-    automationName: 'espresso',
-    newCommandTimeout: 0,
-    skipUnlock: true,
-    fullReset: false,
-    forceEspressoRebuild: true,
-    showGradleLog: true,
-    espressoBuildConfig: '{"additionalAndroidTestDependencies": ["androidx.lifecycle:lifecycle-extensions:2.2.0", "androidx.activity:activity:1.3.1", "androidx.fragment:fragment:1.3.4"]}'
-  }
-end
